4815ae92ba133603bc3b0aea706ccfd42f55af3a
Jean1315 <58617884+Jean1315@users.noreply.github.com>
2021-08-03 19:54:25 -0300

    Add files via upload

---
+++ Senac_System.sql
@@ -0,0 +1,263 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 04-Ago-2021 às 00:51
+-- Versão do servidor: 10.4.18-MariaDB
+-- versão do PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `cadastro_aluno`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aluno`
+--
+
+CREATE TABLE `aluno` (
+  `idAluno` int(11) NOT NULL,
+  `nomeAluno` varchar(50) NOT NULL,
+  `idadeAluno` varchar(3) NOT NULL,
+  `idSexo` int(11) NOT NULL,
+  `idMunicipio` int(11) NOT NULL,
+  `idEstado` int(11) NOT NULL,
+  `idPais` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `aluno`
+--
+
+INSERT INTO `aluno` (`idAluno`, `nomeAluno`, `idadeAluno`, `idSexo`, `idMunicipio`, `idEstado`, `idPais`) VALUES
+(27, 'Jean Henrique', '19', 1, 1, 1, 1),
+(28, 'Roberta', '22', 2, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estado`
+--
+
+CREATE TABLE `estado` (
+  `idEstado` int(11) NOT NULL,
+  `descricaoEstado` varchar(50) NOT NULL,
+  `idPais` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `estado`
+--
+
+INSERT INTO `estado` (`idEstado`, `descricaoEstado`, `idPais`) VALUES
+(1, 'Rio Grande do Sul', 1),
+(2, 'Buenos Aires', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `login`
+--
+
+CREATE TABLE `login` (
+  `idLogin` int(3) NOT NULL,
+  `login` varchar(25) NOT NULL,
+  `senha` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `login`
+--
+
+INSERT INTO `login` (`idLogin`, `login`, `senha`) VALUES
+(1, 'master@master.com', 'master');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `municipio`
+--
+
+CREATE TABLE `municipio` (
+  `idMunicipio` int(11) NOT NULL,
+  `descricaoMunicipio` varchar(50) NOT NULL,
+  `idEstado` int(11) NOT NULL,
+  `idPais` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `municipio`
+--
+
+INSERT INTO `municipio` (`idMunicipio`, `descricaoMunicipio`, `idEstado`, `idPais`) VALUES
+(1, 'Santa Cruz do Sul', 1, 1),
+(2, 'Vera Cruz', 1, 1),
+(3, 'La Plata', 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pais`
+--
+
+CREATE TABLE `pais` (
+  `idPais` int(11) NOT NULL,
+  `descricaoPais` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `pais`
+--
+
+INSERT INTO `pais` (`idPais`, `descricaoPais`) VALUES
+(1, 'Brasil'),
+(2, 'Argentina ');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sexo`
+--
+
+CREATE TABLE `sexo` (
+  `idSexo` int(11) NOT NULL,
+  `descricaoSexo` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `sexo`
+--
+
+INSERT INTO `sexo` (`idSexo`, `descricaoSexo`) VALUES
+(1, 'Masculino'),
+(2, 'Feminino');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `aluno`
+--
+ALTER TABLE `aluno`
+  ADD PRIMARY KEY (`idAluno`),
+  ADD KEY `fk_aluno_sexo` (`idSexo`),
+  ADD KEY `fr_aluno_municipio` (`idMunicipio`),
+  ADD KEY `fk_aluno_pais` (`idPais`),
+  ADD KEY `fk_aluno_estado` (`idEstado`);
+
+--
+-- Índices para tabela `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`idEstado`),
+  ADD KEY `fk_estado_pais` (`idPais`);
+
+--
+-- Índices para tabela `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`idLogin`);
+
+--
+-- Índices para tabela `municipio`
+--
+ALTER TABLE `municipio`
+  ADD PRIMARY KEY (`idMunicipio`),
+  ADD KEY `fk_municipio_estado` (`idEstado`),
+  ADD KEY `fk_municipio_pais` (`idPais`);
+
+--
+-- Índices para tabela `pais`
+--
+ALTER TABLE `pais`
+  ADD PRIMARY KEY (`idPais`);
+
+--
+-- Índices para tabela `sexo`
+--
+ALTER TABLE `sexo`
+  ADD PRIMARY KEY (`idSexo`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `aluno`
+--
+ALTER TABLE `aluno`
+  MODIFY `idAluno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+
+--
+-- AUTO_INCREMENT de tabela `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `idEstado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `login`
+--
+ALTER TABLE `login`
+  MODIFY `idLogin` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `municipio`
+--
+ALTER TABLE `municipio`
+  MODIFY `idMunicipio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `pais`
+--
+ALTER TABLE `pais`
+  MODIFY `idPais` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `sexo`
+--
+ALTER TABLE `sexo`
+  MODIFY `idSexo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `aluno`
+--
+ALTER TABLE `aluno`
+  ADD CONSTRAINT `fk_aluno_estado` FOREIGN KEY (`idEstado`) REFERENCES `estado` (`idEstado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_aluno_pais` FOREIGN KEY (`idPais`) REFERENCES `pais` (`idPais`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_aluno_sexo` FOREIGN KEY (`idSexo`) REFERENCES `sexo` (`idSexo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fr_aluno_municipio` FOREIGN KEY (`idMunicipio`) REFERENCES `municipio` (`idMunicipio`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `estado`
+--
+ALTER TABLE `estado`
+  ADD CONSTRAINT `fk_estado_pais` FOREIGN KEY (`idPais`) REFERENCES `pais` (`idPais`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `municipio`
+--
+ALTER TABLE `municipio`
+  ADD CONSTRAINT `fk_municipio_estado` FOREIGN KEY (`idEstado`) REFERENCES `estado` (`idEstado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_municipio_pais` FOREIGN KEY (`idPais`) REFERENCES `pais` (`idPais`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
